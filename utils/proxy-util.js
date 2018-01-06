f8dd1ecee4da71b1119e76c9bf45dd0fbac37e3b
flaff <flaffy90@gmail.com>
2018-01-06 21:41:58 +0100

    number exception on response fix

--- utils/proxy-util.js
+++ utils/proxy-util.js
@@ -1,14 +1,18 @@
+function safeParse(data) {
+    return data && data.constructor && data.constructor === Number ? String(data) : data;
+}
+
 function proxyRequest(proxyResponse, modifier) {
     return function (apiResponse) {
         modifier && modifier(apiResponse, proxyResponse);
-        proxyResponse.send(apiResponse.data);
+        proxyResponse.send(safeParse(apiResponse.data));
     }
 }
 
 function proxyRequestFailHandler(proxyResponse, modifier) {
     return function (apiResponse) {
         modifier && modifier(apiResponse, proxyResponse);
-        proxyResponse.send(JSON.stringify(apiResponse));
+        proxyResponse.send(safeParse(apiResponse.data));
     }
 }
 
